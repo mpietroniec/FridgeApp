bf7566f6cfb07c9cc37a3351d10712d17a7dd44a
Marcin <mpietroniec@gmail.com>
2021-04-04 11:52:38 +0200

    Update feature added.

--- app/src/main/java/com/project/fridgeapp/update/UpdateFridgeProductActivity.java
+++ app/src/main/java/com/project/fridgeapp/update/UpdateFridgeProductActivity.java
@@ -2,17 +2,26 @@ package com.project.fridgeapp.update;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.project.fridgeapp.R;
+import com.project.fridgeapp.database.DatabaseHelper;
 import com.project.fridgeapp.entities.FridgeProduct;
+import com.project.fridgeapp.entities.ShoppingListItem;
+
+import java.util.List;
 
 public class UpdateFridgeProductActivity extends AppCompatActivity {
     private EditText etxtUpdateName, etxtUpdateAmount;
     private Button btnUpdate;
+    private DatabaseHelper database;
+    private Context context;
+    private List<ShoppingListItem> shoppingListItemsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +31,27 @@ public class UpdateFridgeProductActivity extends AppCompatActivity {
         Intent intent = getIntent();
         FridgeProduct fridgeProduct = intent.getParcelableExtra("Fridge Product");
 
-        String productName = fridgeProduct.getFridgeProductName();
-        int productAmount = fridgeProduct.getFridgeProductAmount();
+        database = DatabaseHelper.getInstance(context);
+
+        long sFridgeID = fridgeProduct.getFridgeID();
+        String sProductName = fridgeProduct.getFridgeProductName();
+        int sAmount = fridgeProduct.getFridgeProductAmount();
 
         etxtUpdateName = findViewById(R.id.etxt_update_name);
-        etxtUpdateName.setText(productName);
+        etxtUpdateName.setText(sProductName);
 
         etxtUpdateAmount = findViewById(R.id.etxt_update_amount);
-        etxtUpdateAmount.setText(String.valueOf(productAmount));
+        etxtUpdateAmount.setText(String.valueOf(sAmount));
+
+        btnUpdate = findViewById(R.id.btn_update_product);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sProductName = etxtUpdateName.getText().toString().trim();
+                int sAmount = Integer.parseInt(etxtUpdateAmount.getText().toString().trim());
+
+                database.fridgeProductDao().update(sFridgeID, sProductName, sAmount);
+            }
+        });
     }
 }
--- app/src/main/java/com/project/fridgeapp/update/UpdateShoppingListItemActivity.java
+++ app/src/main/java/com/project/fridgeapp/update/UpdateShoppingListItemActivity.java
@@ -2,15 +2,25 @@ package com.project.fridgeapp.update;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.project.fridgeapp.R;
+import com.project.fridgeapp.database.DatabaseHelper;
 import com.project.fridgeapp.entities.ShoppingListItem;
+
+import java.util.List;
 
 public class UpdateShoppingListItemActivity extends AppCompatActivity {
     private EditText etxtUpdateShoppingListItemName, etxtUpdateShoppingListItemAmount, etxtUpdateShoppingListItemShopName;
+    private Button btnUpdate;
+    private DatabaseHelper database;
+    private Context context;
+    private List<ShoppingListItem> shoppingListItemsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,9 @@ public class UpdateShoppingListItemActivity extends AppCompatActivity {
         Intent intent = getIntent();
         ShoppingListItem shoppingListItem = intent.getParcelableExtra("Shopping List Item");
 
+        database = DatabaseHelper.getInstance(context);
+
+        long sShoppingListItemID = shoppingListItem.getShoppingListItemID();
         String shoppingListItemName = shoppingListItem.getShoppingListItemName();
         int shoppingListItemAmount = shoppingListItem.getShoppingListItemAmount();
         String shoppingListItemShopName = shoppingListItem.getShoppingListItemShopName();
@@ -32,5 +45,17 @@ public class UpdateShoppingListItemActivity extends AppCompatActivity {
 
         etxtUpdateShoppingListItemShopName = findViewById(R.id.etxt_update_shopping_list_shop_name);
         etxtUpdateShoppingListItemShopName.setText(shoppingListItemShopName);
+
+        btnUpdate = findViewById(R.id.btn_update_shopping_list);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sShoppingListItemName = etxtUpdateShoppingListItemName.getText().toString().trim();
+                int sShoppingListItemAmount = Integer.parseInt(etxtUpdateShoppingListItemAmount.getText().toString().trim());
+                String sShoppingListItemShopName = etxtUpdateShoppingListItemShopName.getText().toString().trim();
+
+                database.shoppingListItemDao().update(sShoppingListItemID, sShoppingListItemName, sShoppingListItemAmount, sShoppingListItemShopName);
+            }
+        });
     }
 }
